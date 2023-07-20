4e64a3ccf73518a0e09fb87a0ffb2704c6e872d6
Rambaud Pierrick <12rambau@users.noreply.github.com>
2023-07-20 13:15:58 +0000

    bump: version 0.0.0 → 0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ from datetime import datetime
 project = "pyGAUL"
 author = "Pierrick Rambaud"
 copyright = f"2020-{datetime.now().year}, {author}"
-release = "0.0.0"
+release = "0.1.0"
 
 # -- General configuration -----------------------------------------------------
 extensions = [
--- pygaul/__init__.py
+++ pygaul/__init__.py
@@ -16,7 +16,7 @@ import pandas as pd
 
 ee.Initialize()
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 __author__ = "Pierrick Rambaud"
 __email__ = "pierrick.rambaud49@gmail.com"
 
