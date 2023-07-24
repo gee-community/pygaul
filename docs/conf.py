81873c305a51d72f56ad634e73cee43e15b1781f
Rambaud Pierrick <12rambau@users.noreply.github.com>
2023-07-24 12:59:44 +0000

    bump: version 0.1.1 → 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import httplib2
 project = "pyGAUL"
 author = "Pierrick Rambaud"
 copyright = f"2020-{datetime.now().year}, {author}"
-release = "0.1.1"
+release = "0.2.0"
 
 # -- General configuration -----------------------------------------------------
 extensions = [
