27dea107011a1085ce20a3b1505117a3026dd874
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2023-08-29 12:18:33 +0000

    feat: add a Names object

--- pygaul/__init__.py
+++ pygaul/__init__.py
@@ -23,6 +23,109 @@ __gaul_continent__ = Path(__file__).parent / "data" / "gaul_continent.json"
 __gaul_asset__ = "FAO/GAUL/2015/level{}"
 
 
+class Names(pd.DataFrame):
+    def __init__(
+        self,
+        name: str = "",
+        admin: str = "",
+        content_level: int = -1,
+        complete: bool = False,
+    ):
+        """Object to handle names of administrative layer using the name or the administrative code.
+
+        Compute a pandas DataFrame of the names as FAO GAUL codes of and administrative region. The region can be requested either by its "name" or its "admin", the lib will identify the corresponding level on the fly. The user can also request for a specific level for its content e.g. get all admin level 1 of a country. If nothing is set we will infer the level of the item and if the level is higher than the found item, it will be ignored. If Nothing is found the method will raise an error.
+
+        Args:
+            name: The name of a administrative area. Cannot be set along with :code:`admin`.
+            admin: The id of an administrative area in the FAO GAUL nomenclature. Cannot be set along with :code:`name`.
+            content_level: The level to use in the final dataset. Default to -1 (use level of the selected area).
+            complete: If True, the method will return all the names of the higher administrative areas. Default to False.
+        """
+        # sanitary check on parameters
+        if name and admin:
+            raise ValueError('"name" and "id" cannot be set at the same time.')
+
+        # if a name or admin number is set, we need to filter the dataset accordingly
+        # if not we will simply consider the world dataset
+        df = pd.read_parquet(__gaul_data__)
+        if name or admin:
+            # set the id we look for and tell the function if its a name or an admin
+            is_name = True if name else False
+            id = name if name else admin
+
+            # read the data and find if the element exist
+            column = "ADM{}_NAME" if is_name else "ADM{}_CODE"
+            is_in = (
+                df.filter([column.format(i) for i in range(3)])
+                .apply(lambda col: col.str.lower())
+                .isin([id.lower()])
+            )
+
+            if not is_in.any().any():
+                # find the 5 closest names/id
+                columns = [
+                    df[column.format(i)].dropna().str.lower().values for i in range(3)
+                ]
+                ids = np.unique(np.concatenate(columns))
+                close_ids = get_close_matches(id.lower(), ids, n=5)
+                if is_name is True:
+                    close_ids = [i.capitalize() for i in close_ids]
+                else:
+                    close_ids = [i.upper() for i in close_ids]
+                raise ValueError(
+                    f'The requested "{id}" is not part of FAO GAUL 2015. The closest '
+                    f'matches are: {", ".join(close_ids)}.'
+                )
+
+            # Get the code of the associated country of the identifed area and the associated level
+            line = is_in[~((~is_in).all(axis=1))].idxmax(1)
+            level = line.iloc[0][3]
+
+            # load the max_level available in the requested area
+            sub_df = df[df[column.format(level)].str.fullmatch(id, case=False)]
+            max_level = next(
+                i for i in reversed(range(3)) if (sub_df[f"ADM{i}_NAME"] != "").any()
+            )
+
+            # get the request level from user
+            content_level, level = int(content_level), int(level)
+            if content_level == -1:
+                content_level = level
+            elif content_level < level:
+                warnings.warn(
+                    f"The requested level ({content_level}) is higher than the area ({level}). "
+                    f"Fallback to {level}."
+                )
+                content_level = level
+
+            if content_level > max_level:
+                warnings.warn(
+                    f"The requested level ({content_level}) is higher than the max level "
+                    f"in this country ({max_level}). Fallback to {max_level}."
+                )
+                content_level = max_level
+
+        else:  # no admin and no name
+            sub_df = df
+            content_level = 0 if content_level == -1 else content_level
+
+        # get the columns name corresponding to the requested level
+        columns = [f"ADM{content_level}_NAME", f"ADM{content_level}_CODE"]
+
+        # the list will contain duplicate as all the smaller admin level will be included
+        sub_df = sub_df.drop_duplicates(subset=columns, ignore_index=True)
+
+        # the list will contain NA as all the bigger admin level will be selected as well
+        # the database is read as pure string so dropna cannot be used
+        # .astype is also a vectorized operation so it goes very fast
+        sub_df = sub_df[sub_df[columns[0]].astype(bool)]
+
+        # filter the df if complete is set to False, the only displayed columns will be the one requested
+        final_df = sub_df if complete is True else sub_df[columns]
+
+        super().__init__(final_df)
+
+
 def get_names(
     name: str = "", admin: str = "", content_level: int = -1, complete: bool = False
 ) -> pd.DataFrame:
